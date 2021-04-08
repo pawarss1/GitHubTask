bdba459bb9206db790590982b446e2968c5e0b09
pawarss1 <pawarss_1@rknec.edu>
2021-04-08 19:11:40 +0530

    Updated!

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import UserRoutes from './Routes/UserRoutes';
-import RepoRoutes from './Routes/RepoRoutes';
-import dbConnector from './Services/DatabaseServices/dbConnector';
+import express from "express";
+import dotenv from "dotenv";
+import UserRoutes from "./Routes/UserRoutes";
+import RepoRoutes from "./Routes/RepoRoutes";
+import dbConnector from "./Services/DatabaseServices/dbConnector";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// Connection to the Database.
+// Connection to the Database. DB used- MongoDB.
 dbConnector();
 
-app.use('/user', UserRoutes);
-app.use('/repo', RepoRoutes);
+app.use("/user", UserRoutes);
+app.use("/repo", RepoRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log('app is listening on port 3000');
+  console.log("app is listening on port 3000");
 });
